bc8716c3f9949dc6b72bf2abd0f4f1ba1302de0e
kyrisss <kornev.roma62@gmail.com>
2022-01-21 08:58:43 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let strN = String(n);
+    let strReverse='';
+    for(let i = strN.length-1; i>=0; i--){
+        strReverse += strN[i];
+    }
+    return parseInt(strReverse);
 }
